46fc6289738897f342e700acb9cc85fa22dbb93f
[hyunba] <dls272@naver.com>
2021-11-29 21:35:51 +0900

    react-for-typescript

---
+++ src/theme.ts
@@ -0,0 +1,11 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+    bgColor: "white",
+    textColor: "black"
+};
+
+export const darkTheme: DefaultTheme = {
+    bgColor: "#111",
+    textColor: "white"
+};
